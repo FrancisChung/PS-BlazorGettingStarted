c9145be08a71803e9aaa0eee75853a1e2765bf93
FrancisChung <francis.c.chung@gmail.com>
2021-06-18 21:48:59 +1000

    GetAllEmployess() Implemented

--- BethanysPieShopHRM.App/BethanysPieShopHRM.App/Services/EmployeeDataService.cs
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Services/EmployeeDataService.cs
@@ -2,12 +2,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BethanysPieShopHRM.App.Services
 {
     public class EmployeeDataService : IEmployeeDataService
     {
+        private readonly HttpClient _httpClient;
+
+        public EmployeeDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public Task<Employee> AddEmployee(Employee employee)
         {
             throw new NotImplementedException();
@@ -18,9 +27,10 @@ namespace BethanysPieShopHRM.App.Services
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Employee>> GetAllEmployees()
+        public async Task<IEnumerable<Employee>> GetAllEmployees()
         {
-            throw new NotImplementedException();
+            return await JsonSerializer.DeserializeAsync<IEnumerable<Employee>>
+                (await _httpClient.GetStreamAsync($"api/employee"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
 
         public Task<Employee> GetEmployeeDetails(int employeeId)
