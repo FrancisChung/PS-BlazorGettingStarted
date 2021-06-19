2a0851d33c199aef09bf566c42b363a9aa07955a
FrancisChung <francis.c.chung@gmail.com>
2021-06-19 16:38:46 +1000

    Remove hard-coded data and related functions

---
+++ bethanyspieshophrm.app/BethanysPieShopHRM.App/Pages/EmployeeOverview.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShopHRM.Shared;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+
+            return base.OnInitializedAsync();
+        }
+
+
+	}
+}
