246d811d3a26c054dc15d4d41d6a2d83dec95b6a
FrancisChung <francis.c.chung@gmail.com>
2021-07-05 20:11:06 +1000

    Employee property and EmployeeDataService defined

---
+++ BethanysPieShopHRM.App/Components/AddEmployeeDialog.cs
@@ -0,0 +1,18 @@
+﻿using BethanysPieShopHRM.App.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.App.Components
+{
+    public partial class AddEmployeeDialog
+    {
+        public Employee Employee { get; set; } = new Employee { CountryId = 1, JobCategoryId = 1, BirthDate = DateTime.Now, JoinedDate = DateTime.Now };
+
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+    }
+}
